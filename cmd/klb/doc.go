4cda1c2c3b2496e963079750cc542eb9d8aaf1cb
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2016-11-26 21:57:26 -0200

    Add some tests and split logic
    Signed-off-by: Tiago Natel de Moura <tiago.natel@neoway.com.br>

---
+++ cmd/klb/doc.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/NeowayLabs/nash/ast"
+	"github.com/NeowayLabs/nash/parser"
+)
+
+func printDoc(stdout, _ io.Writer, docs []*ast.CommentNode, fn *ast.FnDeclNode) {
+	fmt.Fprintf(stdout, "fn %s(%s)\n", fn.Name(), strings.Join(fn.Args(), ", "))
+
+	for _, doc := range docs {
+		fmt.Fprintf(stdout, "\t%s\n", doc.String()[2:])
+	}
+}
+
+func lookFn(stdout, stderr io.Writer, fname string, pack string, fun string) bool {
+	content, err := ioutil.ReadFile(fname)
+
+	if err != nil {
+		panic(err)
+	}
+
+	parser := parser.NewParser(fname, string(content))
+
+	tree, err := parser.Parse()
+
+	if err != nil {
+		return false
+	}
+
+	nodelen := len(tree.Root.Nodes)
+
+	for i, j := 0, 1; j < nodelen; i, j = i+1, j+1 {
+		var comments []*ast.CommentNode
+
+		node := tree.Root.Nodes[i]
+		next := tree.Root.Nodes[j]
+
+		if node.Type() == ast.NodeComment {
+			comments = append(comments, node.(*ast.CommentNode))
+			last := node
+
+			// process comments
+			for i = i + 1; i < nodelen-1; i++ {
+				node = tree.Root.Nodes[i]
+
+				if node.Type() == ast.NodeComment &&
+					node.Line() == last.Line()+1 {
+					comments = append(comments, node.(*ast.CommentNode))
+					last = node
+				} else {
+					break
+				}
+			}
+
+			j = i
+			i--
+			next = tree.Root.Nodes[j]
+
+			if next.Line() != last.Line()+1 {
+				comments = []*ast.CommentNode{}
+			}
+		} else if node.Type() == ast.NodeFnDecl {
+			fn := node.(*ast.FnDeclNode)
+
+			if fn.Name() == fun {
+				printDoc(stdout, stderr, []*ast.CommentNode{}, fn)
+				return true
+			}
+
+			continue
+		} else {
+			continue
+		}
+
+		if next.Type() == ast.NodeFnDecl {
+			fn := next.(*ast.FnDeclNode)
+
+			if fn.Name() == fun {
+				printDoc(stdout, stderr, comments, next.(*ast.FnDeclNode))
+
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func docUsage(out io.Writer) {
+	fmt.Fprintf(out, "Usage: %s doc <package>.<fn name>\n", os.Args[0])
+}
+
+func doc(stdout, stderr io.Writer, args []string) error {
+	if len(args) < 2 {
+		docUsage(stderr)
+		return nil
+	}
+
+	packfn := args[1]
+	parts := strings.Split(packfn, ".")
+
+	if len(parts) != 2 {
+		docUsage(stderr)
+		return nil
+	}
+
+	pack := parts[0]
+	funName := parts[1]
+
+	nashpath := os.Getenv("NASHPATH")
+
+	if nashpath == "" {
+		homepath := os.Getenv("HOME")
+
+		if homepath == "" {
+			return fmt.Errorf("NASHPATH not set...\n")
+		}
+
+		fmt.Fprintf(stderr, "NASHPATH not set. Using ~/.nash\n")
+
+		nashpath = homepath + "/.nash"
+	}
+
+	err := filepath.Walk(nashpath+"/lib", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		dirpath := filepath.Dir(path)
+		dirname := filepath.Base(dirpath)
+		ext := filepath.Ext(path)
+
+		if ext != "" && ext != ".sh" && dirname != pack {
+			return nil
+		}
+
+		if found := lookFn(stdout, stderr, path, pack, funName); found {
+			return errors.New("found")
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		if err.Error() == "found" {
+			return nil
+		}
+
+		return err
+	}
+
+	return fmt.Errorf("Function %s.%s not found", pack, funName)
+}
--- cmd/klb/main.go
+++ cmd/klb/main.go
@@ -1,159 +1,35 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
-	"path/filepath"
-	"strings"
-
-	"github.com/NeowayLabs/nash/ast"
-	"github.com/NeowayLabs/nash/parser"
 )
 
-func printDoc(docs []*ast.CommentNode, fn *ast.FnDeclNode) {
-	fmt.Printf("fn %s(%s)\n", fn.Name(), strings.Join(fn.Args(), ", "))
-
-	for _, doc := range docs {
-		fmt.Printf("\t%s\n", doc.String()[2:])
-	}
-}
-
-func lookFn(fname string, pack string, fun string) bool {
-	content, err := ioutil.ReadFile(fname)
-
-	if err != nil {
-		panic(err)
-	}
-
-	parser := parser.NewParser(fname, string(content))
-
-	tree, err := parser.Parse()
-
-	if err != nil {
-		return false
-	}
-
-	nodelen := len(tree.Root.Nodes)
-
-	for i, j := 0, 1; j < nodelen; i, j = i+1, j+1 {
-		var comments []*ast.CommentNode
-
-		node := tree.Root.Nodes[i]
-		next := tree.Root.Nodes[j]
-
-		if node.Type() == ast.NodeComment {
-			comments = append(comments, node.(*ast.CommentNode))
-			last := node
-
-			// process comments
-			for i = i + 1; i < nodelen-1; i++ {
-				node = tree.Root.Nodes[i]
-
-				if node.Type() == ast.NodeComment &&
-					node.Line() == last.Line()+1 {
-					comments = append(comments, node.(*ast.CommentNode))
-					last = node
-				} else {
-					break
-				}
-			}
-
-			j = i
-			i--
-			next = tree.Root.Nodes[j]
-
-			if next.Line() != last.Line()+1 {
-				comments = []*ast.CommentNode{}
-			}
-		} else if node.Type() == ast.NodeFnDecl {
-			fn := node.(*ast.FnDeclNode)
-
-			if fn.Name() == fun {
-				printDoc([]*ast.CommentNode{}, fn)
-				return true
-			}
-
-			continue
-		} else {
-			continue
-		}
-
-		if next.Type() == ast.NodeFnDecl {
-			fn := next.(*ast.FnDeclNode)
-
-			if fn.Name() == fun {
-				printDoc(comments, next.(*ast.FnDeclNode))
-
-				return true
-			}
-		}
-	}
-
-	return false
-}
-
-func usage() {
-	fmt.Printf("%s doc <package>.<fn name>\n", os.Args[0])
-	os.Exit(1)
+func usage(out io.Writer) {
+	fmt.Fprintf(out, "Usage: %s <command> <args>\n")
+	fmt.Fprintf(out, "Commands available: \n")
+	fmt.Fprintf(out, "  doc\tDocumentation of nash libraries\n")
 }
 
 func main() {
-	if len(os.Args) < 3 {
-		usage()
+	if err := do(os.Args, os.Stdout, os.Stderr); err != nil {
+		fmt.Printf("error: %s\n", err.Error())
+		os.Exit(1)
 	}
+}
 
-	packfn := os.Args[2]
-	parts := strings.Split(packfn, ".")
-
-	if len(parts) != 2 {
-		usage()
-	}
-
-	pack := parts[0]
-	funName := parts[1]
-
-	nashpath := os.Getenv("NASHPATH")
-
-	if nashpath == "" {
-		homepath := os.Getenv("HOME")
-
-		if homepath == "" {
-			fmt.Fprintf(os.Stderr, "NASHPATH not set...\n")
-			os.Exit(1)
-		}
-
-		fmt.Fprintf(os.Stderr, "NASHPATH not set. Using ~/.nash\n")
-
-		nashpath = homepath + "/.nash"
-	}
-
-	err := filepath.Walk(nashpath+"/lib", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		dirpath := filepath.Dir(path)
-		dirname := filepath.Base(dirpath)
-		ext := filepath.Ext(path)
-
-		if ext != "" && ext != ".sh" && dirname != pack {
-			return nil
-		}
-
-		if found := lookFn(path, pack, funName); found {
-			return errors.New("found")
-		}
-
+func do(args []string, stdout, stderr io.Writer) error {
+	if len(os.Args) < 2 {
+		usage(os.Stderr)
 		return nil
-	})
-
-	if err != nil && err.Error() != "found" {
-		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 	}
+
+	if os.Args[1] == "doc" {
+		return doc(stdout, stderr, os.Args[1:])
+	}
+
+	fmt.Printf("Invalid command: %s\n", os.Args[1])
+	usage(os.Stderr)
+	return nil
 }
