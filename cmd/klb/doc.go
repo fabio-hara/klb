64b5aea9ab116ea8f1546409a7cf8bfbda3cf655
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2016-11-27 00:05:11 -0200

    improved core logic
    Signed-off-by: Tiago Natel de Moura <tiago.natel@neoway.com.br>

--- cmd/klb/doc.go
+++ cmd/klb/doc.go
@@ -24,7 +24,8 @@ func lookFn(stdout, stderr io.Writer, fname string, pack string, fun string) boo
 	content, err := ioutil.ReadFile(fname)
 
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(stderr, "error: %s\n", err.Error())
+		return false
 	}
 
 	parser := parser.NewParser(fname, string(content))
@@ -60,35 +61,33 @@ func lookFn(stdout, stderr io.Writer, fname string, pack string, fun string) boo
 				}
 			}
 
-			j = i
 			i--
+			j = i + 1
 			next = tree.Root.Nodes[j]
 
 			if next.Line() != last.Line()+1 {
 				comments = []*ast.CommentNode{}
 			}
+
+			if next.Type() == ast.NodeFnDecl {
+				fn := next.(*ast.FnDeclNode)
+
+				if fn.Name() == fun {
+					printDoc(stdout, stderr, comments, next.(*ast.FnDeclNode))
+
+					return true
+				}
+			}
 		} else if node.Type() == ast.NodeFnDecl {
 			fn := node.(*ast.FnDeclNode)
 
 			if fn.Name() == fun {
+				// found func, but no docs :-(
 				printDoc(stdout, stderr, []*ast.CommentNode{}, fn)
 				return true
 			}
-
-			continue
-		} else {
-			continue
 		}
 
-		if next.Type() == ast.NodeFnDecl {
-			fn := next.(*ast.FnDeclNode)
-
-			if fn.Name() == fun {
-				printDoc(stdout, stderr, comments, next.(*ast.FnDeclNode))
-
-				return true
-			}
-		}
 	}
 
 	return false
@@ -96,6 +95,30 @@ func lookFn(stdout, stderr io.Writer, fname string, pack string, fun string) boo
 
 func docUsage(out io.Writer) {
 	fmt.Fprintf(out, "Usage: %s doc <package>.<fn name>\n", filepath.Base(os.Args[0]))
+}
+
+func walk(stdout, stderr io.Writer, nashpath, pkg, fnName string) error {
+	return filepath.Walk(nashpath+"/lib", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		dirpath := filepath.Dir(path)
+		dirname := filepath.Base(dirpath)
+		ext := filepath.Ext(path)
+
+		if ext != "" && ext != ".sh" && dirname != pkg {
+			return nil
+		}
+
+		lookFn(stdout, stderr, path, pkg, fnName)
+
+		return nil
+	})
 }
 
 func doc(stdout, stderr io.Writer, args []string) error {
@@ -112,8 +135,8 @@ func doc(stdout, stderr io.Writer, args []string) error {
 		return nil
 	}
 
-	pack := parts[0]
-	funName := parts[1]
+	pkg := parts[0]
+	fnName := parts[1]
 
 	nashpath := os.Getenv("NASHPATH")
 
@@ -129,25 +152,5 @@ func doc(stdout, stderr io.Writer, args []string) error {
 		nashpath = homepath + "/.nash"
 	}
 
-	return filepath.Walk(nashpath+"/lib", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		dirpath := filepath.Dir(path)
-		dirname := filepath.Base(dirpath)
-		ext := filepath.Ext(path)
-
-		if ext != "" && ext != ".sh" && dirname != pack {
-			return nil
-		}
-
-		lookFn(stdout, stderr, path, pack, funName)
-
-		return nil
-	})
+	return walk(stdout, stderr, nashpath, pkg, fnName)
 }
