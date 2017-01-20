e52142aaecc5d622ff5407a44fc0e41e6c0b7491
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-01-20 21:04:37 -0200

    Funny behaviour with split

--- tools/azure/cleanup.sh
+++ tools/azure/cleanup.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env nash
 
-resgroups <= azure group list --json
-length <= echo $resgroups | jq ". | length"
+resgroups <= azure group list
+resgroups <= split($resgroups, "\n")
 
-echo $length
-#for resgroup in $resgroups {
-    #echo $resgroup
-#}
+for resgroup in $resgroups {
+    filtered <= echo $resgroup | -grep "klb"
+    echo "filtered["+$filtered+"]"
+    splitted <= split($filtered, " ")
+    size <= len($splitted)
+    echo "size: " + $size
+    for word in $splitted {
+        echo "word[" + $word + "]"
+    }
+}
