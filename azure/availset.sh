7bfe18e2ede65a8e1f178824dbca98dad9d6fc42
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-01-23 19:29:58 -0200

    Add function to check if availset exists

--- azure/availset.sh
+++ azure/availset.sh
@@ -8,6 +8,17 @@ fn azure_availset_create(name, group, location) {
 	azure availset create --name $name --resource-group $group --location $location
 }
 
+# azure_availset_exists checks if the given
+# availset exists inside the provided resource group.
+# returns "true" if it exists, "false" otherwise.
+fn azure_availset_exists(name, group) {
+        azure availset list $group | -grep -w $name
+        if $status == "0" {
+                return "true"
+        }
+        return "false"
+}
+
 # azure_availset_delete deletes a exit `availability set`.
 # `name` is the availability set name
 # `group` is the resource group name
